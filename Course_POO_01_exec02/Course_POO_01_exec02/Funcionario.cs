b5e13124f65ef2629c89d15ea4125f1f0c4a4a5f
Gabriel Rocha <gabrielrm761@gmail.com>
2019-12-30 09:18:32 -0300

    Primeiros exercícios do curso

---
+++ Course_POO_01_exec02/Course_POO_01_exec02/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_POO_01_exec02
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double Salario;
+    }
+}

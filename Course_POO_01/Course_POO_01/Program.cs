11aafe47566c462617883d33bc20da3ba4f0b618
Gabriel Rocha <gabrielrm761@gmail.com>
2019-12-30 09:20:17 -0300

    Primeiro projeto
    já implementado o método na classe piramide para delegar a responsabilidade

---
+++ Course_POO_01/Course_POO_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_POO_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo x, y;
+
+            x = new Triangulo();
+            y = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triânguo X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triânguo Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
+            Console.WriteLine("Área de X: " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y: " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else if(areaY > areaX)
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+            else
+            {
+                Console.WriteLine("Áreas de mesmo tamanho.");
+            }
+        }
+    }
+}

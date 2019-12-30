1d54ad728c03f393603f29ca3144d188675337fd
Gabriel Rocha <gabrielrm761@gmail.com>
2019-12-30 11:55:15 -0300

    Segundo Projeto

---
+++ Course_POO_02/Course_POO_02/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_POO_02
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmStoque()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}

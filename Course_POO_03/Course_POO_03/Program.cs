b490616a4227c9a4b213a9e1cea8834054eeed2f
Gabriel Rocha <gabrielrm761@gmail.com>
2020-01-01 12:34:20 -0300

    3° projeto
    explicação de membros estáticos, usando métodos na própria classe do programa

---
+++ Course_POO_03/Course_POO_03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_POO_03
+{
+    class Program
+    {
+        static double Pi = 3.14;
+        static void Main(string[] args)
+        {
+            Console.Write("Insira o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Circunferencia(raio);
+            double volume = Volume(raio);
+
+            Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+
+        static double Circunferencia(double r)
+        {
+            return 2 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4 / 3 * Pi * r * r * r;
+        }
+    }
+}

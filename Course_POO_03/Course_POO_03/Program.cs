2e3bfcbe0f50555fe943a9ea95f3079f86e0e21d
Gabriel Rocha <gabrielrm761@gmail.com>
2020-01-01 12:47:45 -0300

    3° projeto
    Atualizando com nova classe, usando metódos e atributo estáticos para que não seja necessário uma instanciação para usar os membros da classe Calculadora.

--- Course_POO_03/Course_POO_03/Program.cs
+++ Course_POO_03/Course_POO_03/Program.cs
@@ -5,28 +5,20 @@ namespace Course_POO_03
 {
     class Program
     {
-        static double Pi = 3.14;
         static void Main(string[] args)
         {
+
+            Calculadora calc = new Calculadora();
+
             Console.Write("Insira o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = Circunferencia(raio);
-            double volume = Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de Pi: " + Pi.ToString("F2", CultureInfo.InvariantCulture));
-        }
-
-        static double Circunferencia(double r)
-        {
-            return 2 * Pi * r;
-        }
-
-        static double Volume(double r)
-        {
-            return 4 / 3 * Pi * r * r * r;
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }

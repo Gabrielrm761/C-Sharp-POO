99e7eca8adcb910b2502f4f5affd6a3abf4b1a87
Gabriel Rocha <gabrielrm761@gmail.com>
2020-01-01 13:10:12 -0300

    Exercício proposto
    Exercício proposto para fixação com base no 3° projeto

---
+++ Course_POO_03_exec01/Course_POO_03_exec01/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_POO_03_exec01
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+        public static double Valor(double cotacao, double dolar)
+        {
+            double total = cotacao * dolar;
+            return total + total * Iof / 100;
+        }
+    }
+}
---
+++ Course_POO_03_exec01/Course_POO_03_exec01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course_POO_03_exec01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dólar? ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double valor = ConversorDeMoeda.Valor(cotacao, dolar);
+
+
+            Console.WriteLine("Valor a ser pago em reais é: " + valor.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

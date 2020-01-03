79e9cbcdf48bccf0ca85c30d46fbab2909656277
Gabriel Rocha <gabrielrm761@gmail.com>
2020-01-03 10:02:00 -0300

    4° Projeto
    Iniciando construtores.

---
+++ Course_POO_04/Course_POO_04/Produto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Course_POO_04
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto() { }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        }
+
+        public double ValorTotalEmStoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade = Quantidade + quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade = Quantidade - quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", " + Quantidade + " unidades, Total: $ "
+                + ValorTotalEmStoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}

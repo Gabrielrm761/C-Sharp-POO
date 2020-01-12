b35c3892d6e9dcac5bd1ef60dcfbe776c29b26b3
Gabriel Rocha <gabrielrm761@gmail.com>
2020-01-12 16:58:09 -0300

    5° Projeto
    Iniciando explicação de tipos referência e tipos valor, utilizando struct exemplificando que uma variável apenas declarada, mas não instanciada, inicia em estado "não atribuída", e o próprio compilador não permite que ela seja acessada.

---
+++ Course_POO_05/Course_POO_05/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_POO_05
+{
+    struct Point
+    {
+
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+
+    }
+}

555242fb3043bee9b978172d3c74b17d9f6c8efc
Gabriel Rocha <gabrielrm761@gmail.com>
2020-01-31 15:37:58 -0300

    Modificador de Parâmetros
    Usando a palavra-chave params, você pode especificar um parâmetro do método que aceita um número variável de argumentos.

---
+++ Course_POO_Modificadores_Parametro/Course_POO_Modificadores_Parametro/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_POO_Modificadores_Parametro
+{
+    class Calculator
+    {
+        public static int Sum(params int[] numbers) //o params vai avisar ao compilador que você quer uma função que possa receber uma quantia varaivel de valores.
+        {
+            int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}

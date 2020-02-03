d111896605a4361803d516801fe4dae79a20de76
Gabriel Rocha <gabrielrm761@gmail.com>
2020-02-03 15:33:24 -0300

    Foreach
    Exemplo de utilização do foreach com vetores

---
+++ Course_POO_foreach/Course_POO_foreach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course_POO_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "Maria", "Alex", "Bob" };
+
+            foreach(string obj in vect)
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}

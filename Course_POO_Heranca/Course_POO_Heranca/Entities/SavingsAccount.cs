9760ca493337f47bf4a63fc2642b668a1bb186f1
Gabriel Rocha <gabrielrm761@gmail.com>
2020-08-22 08:13:29 -0300

    sealed
    Utilização da palavra "sealed" mostrando como é usado e porque

--- Course_POO_Heranca/Course_POO_Heranca/Entities/SavingsAccount.cs
+++ Course_POO_Heranca/Course_POO_Heranca/Entities/SavingsAccount.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Course_POO_Heranca.Entities
 {
-    class SavingsAccount : Account
+    class SavingsAccount : Account //a palavra"sealed" é usada antes do "class" para impedir que a classe seja herdada por uma subclasse.
     {
         public double InterestRate { get; set; }
 
@@ -23,10 +23,22 @@ namespace Course_POO_Heranca.Entities
             Balance += Balance * InterestRate;
         }
 
-        public override void Withdraw(double amount) // Usando o override, você reimplementa na subclasse a operação existente na superclasse, para dar um comportamento diferente. 
+        public sealed override void Withdraw(double amount) // Usando o override, você reimplementa na subclasse a operação existente na superclasse, para dar um comportamento diferente. 
         {
             base.Withdraw(amount);
             Balance -= 2.0;
         }
+        // Usando a palavra sealed em um método já sobrescrito(override), evita que ele seja sobrescrita novamente por uma subclasse
+
+        /* Motivos para utilizar o "sealed"
+         * • Segurança: dependendo das regras do negócio, às vezes é desejável
+                        garantir que uma classe não seja herdada, ou que um método não
+                        seja sobreposto.
+                        • Geralmente convém selar métodos sobrepostos, pois sobreposições múltiplas
+                        podem ser uma porta de entrada para inconsistências
+           • Performance: atributos de tipo de uma classe selada são analisados
+                          de forma mais rápida em tempo de execução.
+                          • Exemplo clássico: string
+         */
     }
 }
---
+++ Course_POO_Heranca/Course_POO_Heranca/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course_POO_Heranca.Entities
+{
+    class SavingsAccountPlus : SavingsAccount
+    {
+
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
